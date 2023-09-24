37e8de3aad43be08da891e275134830d5a67ea7e
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-09-24 16:02:28 +1000

    Refactor prac 1 exercises

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,15 +2,18 @@ MINIMUM_LENGTH = 5
 
 
 def main():
+    """check password and print asterisks"""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """print asterisks"""
     print("*" * len(password))
 
 
 def get_password():
+    """Check password length"""
     password = input(f"Enter a password with at least {MINIMUM_LENGTH} characters: ")
     while len(password) < MINIMUM_LENGTH:
         print("password invalid")
--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score < 50:
-        print("bad")
-    elif score < 90:
-        print("Passable")
+
+def main():
+    """Get users score and print status"""
+    score = float(input("Enter score: "))
+    print(determine_grade_status(score))
+    print(determine_grade_status(random.randint(0, 100)))
+
+
+def determine_grade_status(score):
+    """determine score status"""
+    if score < 0 or score > 100:
+        return "Invalid score"
     else:
-        print("Excellent")
+        if score < 50:
+            return "bad"
+        elif score < 90:
+            return "Passable"
+        else:
+            return "Excellent"
+
+
+main()
+
