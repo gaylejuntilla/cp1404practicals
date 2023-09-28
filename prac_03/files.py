41b3616499d744c83ad9dc267d579c6bf6f32169
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-09-28 13:07:33 +1000

    Add the first 2 lines of number.txt

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,10 +1,17 @@
-out_file = open("name.txt", 'w')
-name = input("Name: ")
-print(name, file=out_file)
-out_file.close()
+# out_file = open("name.txt", 'w')
+# name = input("Name: ")
+# print(name, file=out_file)
+# out_file.close()
+#
+# FILENAME = "name.txt"
+# in_file = open(FILENAME, 'r')
+# name = in_file.read()
+# in_file.close()
+# print(f"Your name is {name}")
 
-FILENAME = "name.txt"
-in_file = open(FILENAME, 'r')
-name = in_file.read()
+in_file = open("numbers.txt", "r")
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
 in_file.close()
-print(f"Your name is {name}")
+print(number1 + number2)
+
