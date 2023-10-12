560151e4f750050a8028b3b0da3a2ae1c04bee80
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-10-12 12:00:51 +1000

    Add hex colours file

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+
+
+COLOUR_TO_CODE = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "	#f0f8ff", "amaranth": "	#e52b50", "amber": "#ffbf00",
+                  "amethyst": "#9966cc", "antiquewhite": "#faebd7", "apricot": "#fbceb1", "armygreen": "#4b5320", "asparagus": "#87a96b"}
