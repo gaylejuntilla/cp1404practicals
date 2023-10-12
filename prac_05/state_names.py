86162973f83c98224001c617e07ef41ecdead879
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-10-12 11:47:32 +1000

    Change while loop using EAFP approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,15 +9,18 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-# state_code = input("Enter short state: ").upper()
-# while state_code != "":
-#     if state_code in CODE_TO_NAME:
-#         print(state_code, "is", CODE_TO_NAME[state_code])
-#     else:
-#         print("Invalid short state")
-#     state_code = input("Enter short state: ").upper()
+# EAFP approach
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
 
 for state, state_name in CODE_TO_NAME.items():
     max_state_length = max(len(state) for state in CODE_TO_NAME.keys())
     max_sate_name_length = max(len(state_name) for state_name in CODE_TO_NAME.values())
     print(f"{state:{max_state_length}} is {state_name:{max_sate_name_length}}")
+
+
