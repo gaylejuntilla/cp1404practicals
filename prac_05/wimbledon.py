f0f0f6a3f0743a977fdfb77f9d9b5f3636a1d321
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-10-14 17:15:07 +1000

    Complete wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,6 +1,28 @@
 def main():
     filename = "wimbledon.csv"
     data = get_data(filename)
+    champion_to_wins, countries = process_data(data)
+    display_results(champion_to_wins, countries)
+
+
+def display_results(champion_to_wins, countries):
+    print("Wimbledon Champions: ")
+    for champion, wins in champion_to_wins.items():
+        print(champion, wins, sep=" ")
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in sorted(countries)))
+
+
+def process_data(data):
+    champion_to_wins = {}
+    countries = set()
+    for record in data:
+        try:
+            countries.add(record[1])
+            champion_to_wins[record[2]] += 1
+        except KeyError:
+            champion_to_wins[record[2]] = 1
+    return champion_to_wins, countries
 
 
 def get_data(filename):
@@ -11,3 +33,6 @@ def get_data(filename):
             parts = line.strip().split(",")
             data.append(parts)
         return data
+
+
+main()
