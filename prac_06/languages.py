2884e17a08b139f8083b600b156397d6f9976d58
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-10-30 14:04:40 +1000

    Complete languages activities by adding docstrings

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,6 +9,7 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
+    """Display dynamic programming languages using the ProgrammingLanguage class"""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,13 +4,16 @@
 class ProgrammingLanguage:
     """Programming language class"""
     def __init__(self, field="", typing="", reflection=True, year=0):
+        """Initialise a Programming language object"""
         self.field = field
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Return boolean of whether language is dynamic"""
         return self.typing == "Dynamic"
 
     def __str__(self):
+        """Return string representation of data in a programming language"""
         return f"{self.field}, {self.typing} typing, Reflection={self.reflection}, First appeared in {self.year}"
