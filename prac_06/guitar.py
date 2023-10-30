eaf8707fd51d58c881918c6e3f5e4142727f593b
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-10-30 14:49:16 +1000

    Complete guitar test by comparing expected and actual values

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -8,14 +8,14 @@ Actual time: Around 30, I forgot
 class Guitar:
     """Guitar class"""
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
         """Return a string representation of Guitar object"""
-        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
         """Return age of guitar from current year"""
