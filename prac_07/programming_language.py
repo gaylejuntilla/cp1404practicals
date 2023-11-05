47ca36dc79d8a1eae5fe27dd1cab816a057a5fc3
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-05 20:21:26 +1000

    Update string method in ProgrammingLanguage class

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -17,7 +17,8 @@ class ProgrammingLanguage:
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, " \
+               f"Pointer Arithmetic={self.pointer_arithmetic}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
