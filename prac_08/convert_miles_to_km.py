c877d2f1c9a5c2d0f6d6e6eb1283264d6bb1bb39
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-12 20:14:16 +1000

    Add increment handling function and widget

--- prac_08/convert_miles_to_km.py
+++ prac_08/convert_miles_to_km.py
@@ -25,5 +25,9 @@ class MilesToKilometers(App):
         number_of_kilometers = number_of_miles * MILES_TO_KM_RATIO
         self.root.ids.output_label.text = str(number_of_kilometers)
 
+    def handle_increment(self, increment_value):
+        number_of_miles = self.get_miles() + increment_value
+        self.root.ids.input_number.text = str(number_of_miles)
+
 
 MilesToKilometers().run()
