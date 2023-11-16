416cf4d10599521d78a70c957cd1578a5d15a4f7
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-16 12:28:23 +1000

    Create unreliable car class

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,21 @@
+import random
+
+from car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """"""
+    def __init__(self, name, fuel, reliability):
+        """"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """"""
+        random_number = random.randint(0, 100)
+        if random_number < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,6 @@
+from car import Car
+from unreliable_car import UnreliableCar
+
+my_car = UnreliableCar("betty", 20, 40)
+my_car.drive(30)
+print(my_car)
