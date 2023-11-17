3f582d712800b810a570f17b77b9503cc7007b55
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-17 16:02:42 +1000

    Update silver service taxi class.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,9 +3,18 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """"""
+    flagfall = 4.50
+
     def __init__(self, fanciness: float, **kwargs):
         """"""
         super().__init__(**kwargs)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
+    def get_fare(self, distance):
+        """"""
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
