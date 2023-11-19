bb8ce6d0453cb98cf137dd26f91b20260db9685a
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-19 13:06:04 +1000

    Update taxi simulator

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -5,3 +5,7 @@ taxi = SilverServiceTaxi(name="Fancy Taxi", fuel=50, fanciness=2)
 taxi.drive(18)
 print(taxi)
 print(taxi.get_fare())
+
+# taxi = SilverServiceTaxi(name="Limo", fuel=100, fanciness=2)
+# taxi.drive(59)
+# print(taxi.get_fare())
--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,17 +6,45 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     taxis = [Taxi(name="Prius", fuel=100), SilverServiceTaxi(name="Limo", fuel=100, fanciness=2),
+             # SilverServiceTaxi class uses key word arguments
              SilverServiceTaxi(name="Hummer", fuel=200, fanciness=4)]
     current_taxi = None
+    total_bill = 0
     print("Lets drive")
     print(MENU)
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            print("Taxis available:")
+            display_taxi_options(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
         elif choice == "d":
-            pass
+            if current_taxi:
+                drive_distance = int(input("Drive how far? "))  # error checking?
+                current_taxi.start_fare()
+                current_taxi.drive(drive_distance)
+                cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${cost:.2f}")
+                total_bill += cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
-    pass
+        print(f"Bill to date is ${total_bill:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+    print(f"Total trip cost: ${total_bill}")
+    print("Taxis are now:")
+    display_taxi_options(taxis)
 
+
+def display_taxi_options(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+main()
