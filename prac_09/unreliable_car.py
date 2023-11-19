37152004a3bfc5c05ace99f36cf7b8028d388e56
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-19 13:40:22 +1000

    Add docstrings to unreliable car

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,18 +1,17 @@
 import random
 
 from car import Car
-from random import randint
 
 
 class UnreliableCar(Car):
-    """"""
+    """UnreliableCar class"""
     def __init__(self, name, fuel, reliability):
-        """"""
+        """Initialise Unreliable car object"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        """"""
+        """Drive a car if a randomly generated number is greater than the cars reliability"""
         random_number = random.randint(0, 100)
         if random_number < self.reliability:
             distance_driven = super().drive(distance)
