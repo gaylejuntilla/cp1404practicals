a3d2e67e2ba8378ec3f493ec540e03fb22c57dc5
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-19 13:50:33 +1000

    Add UnreliableCar test program to main

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,6 +1,10 @@
-from car import Car
 from unreliable_car import UnreliableCar
 
-my_car = UnreliableCar("betty", 20, 40)
-my_car.drive(30)
-print(my_car)
+
+def main():
+    my_car = UnreliableCar("betty", 20, 40)
+    my_car.drive(30)
+    print(my_car)
+
+
+main()
