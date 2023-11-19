d6efc559eb0900dbef75d2c691bc7c53db0fd8a7
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-19 13:51:45 +1000

    Add SilverServiceTaxi test program to main

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,11 +1,11 @@
 from silver_service_taxi import SilverServiceTaxi
 
 
-taxi = SilverServiceTaxi(name="Fancy Taxi", fuel=50, fanciness=2)
-taxi.drive(18)
-print(taxi)
-print(taxi.get_fare())
+def main():
+    taxi = SilverServiceTaxi(name="Fancy Taxi", fuel=50, fanciness=2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
 
-# taxi = SilverServiceTaxi(name="Limo", fuel=100, fanciness=2)
-# taxi.drive(59)
-# print(taxi.get_fare())
+
+main()
